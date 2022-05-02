c4ee3c4590a908f1450bf9f1ce6c80b6c3415d0c
Shoma <shomasdutt@gmail.com>
2022-05-02 15:20:10 +0100

    user and admin logins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
 
     <!-- Styles -->
 
-    <link href="{{'frontend/css/bootstrap5.css' }}" rel="stylesheet">
-    <link href="{{ 'frontend/css/custom.css' }}" rel="stylesheet">
+    <link href="{{asset('frontend/css/bootstrap5.css') }}" rel="stylesheet">
+    <link href="{{asset('frontend/css/custom.css') }}" rel="stylesheet">
 
 </head>
 
@@ -57,24 +57,15 @@
                         @endif
                         @else
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">
+                                                    
+                                    {{ __('Profile') }}
+                                </a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
@@ -96,7 +87,7 @@
         </main>
     </div>
     <!-- Scripts -->
-    <script src="{{ 'frontend/js/bootstrap.bundle.min.js' }}" defer></script>
+    <script src="{{asset('frontend/js/bootstrap.min.js') }}" defer></script>
 </body>
 
 </html>
